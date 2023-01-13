a80197522c071a5d3934f08158aaa35f3c06fc49
jcsestilo <jsestilo@up.edu.ph>
2023-01-13 22:28:00 +0800

    Updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../assets/logo.png';
 
 class NavigationBar extends React.Component{
     render(){
         return(
             <div class="navbar">
                 <nav class = "navbar-nav">
+                    <img src={logo}></img>
                     <button class="mobile-nav-toggle" aria-controls='primary-nav' aria-expanded='false'></button>
                 
                     <ul id='primary-nav' class='primary-nav' data-visible='false'>
